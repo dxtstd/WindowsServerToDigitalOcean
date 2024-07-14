baaf95b72aa64c63e459a1fe3d142b5713bb0e05
dxtstd <dxtstd>
2024-07-14 22:09:53 +0800

    first commit

---
+++ setup _2.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+#
+#
+# SOURCE CODE ASLI >> nixpoin.com & https://github.com/aurielly/DigitalOceanWindowsInstaller
+# dimodifikasi oleh dxtstd
+
+echo "Pilih OS yang ingin anda install"
+echo "  1) Windows 2022"
+
+read -p "Pilih [1]: " PILIHOS
+
+case "$PILIHOS" in
+  1|"") PILIHOS=""
+  *) echo "Pilihan salah, kode dihentikan..."; exit ;;
+esac
+
+IP_4=$(curl -4 -s ifconfig.me)
+GW=$(ip route | awk '/default/ { print $3 }')
+
+cat >/tmp/net.bat <<EOF
+
+@ECHO OFF
+cd.>%windir%\GetAdmin
+if exist %windir%\GetAdmin (del /f /q "%windir%\GetAdmin") else (
+echo CreateObject^("Shell.Application"^).ShellExecute "%~s0", "%*", "", "runas", 1 >> "%temp%\Admin.vbs"
+"%temp%\Admin.vbs"
+del /f /q "%temp%\Admin.vbs"
+exit /b 2)
+
+for /f "tokens=3*" %%i in ('netsh interface show interface ^|findstr /I /R "Local.* Ethernet Ins*"') do (set InterfaceName=%%j)
+netsh -c interface ip set address name="Ethernet Instance 0 2" source=static address=$IP_4 mask=255.255.240.0 gateway=$GW
+netsh -c interface ip add dnsservers name="Ethernet Instance 0 2" address=8.8.8.8 index=1 validate=no
+netsh -c interface ip add dnsservers name="Ethernet Instance 0 2" address=8.8.4.4 index=2 validate=no
+
+cd /d "%ProgramData%/Microsoft/Windows/Start Menu/Programs/Startup"
+del /f /q net.bat
+exit
+
+EOF
+
+cat >/tmp/dpart.bat<<EOF
+
+@ECHO OFF
+echo JENDELA INI JANGAN DITUTUP
+echo BANYAK AKAN KONFIGURASI DISINI
+
+cd.>%windir%\GetAdmin
+if exist %windir%\GetAdmin (del /f /q "%windir%\GetAdmin") else (
+echo CreateObject^("Shell.Application"^).ShellExecute "%~s0", "%*", "", "runas", 1 >> "%temp%\Admin.vbs"
+"%temp%\Admin.vbs"
+del /f /q "%temp%\Admin.vbs"
+exit /b 2)
+
+ECHO SELECT VOLUME=%%SystemDrive%% > "%SystemDrive%\diskpart.extend"
+ECHO EXTEND >> "%SystemDrive%\diskpart.extend"
+START /WAIT DISKPART /S "%SystemDrive%\diskpart.extend"
+
+del /f /q "%SystemDrive%\diskpart.extend"
+cd /d "%ProgramData%/Microsoft/Windows/Start Menu/Programs/Startup"
+del /f /q dpart.bat
+timeout 50 >nul
+echo JENDELA INI JANGAN DITUTUP
+exit
+
+EOF
+
+wget --no-check-certificate -O- $PILIHOS | gunzip | dd of=/dev/vda bs=3M status=progress
+
+mount.ntfs-3g /dev/vda2 /mnt
+cd "/mnt/ProgramData/Microsoft/Windows/Start Menu/Programs/"
+cd Start* || cd start*; \
+cp -f /tmp/net.bat net.bat
+cp -f /tmp/dpart.bat dpart.bat
+
+echo 'Your server will turning off in 3 second'
+sleep 3
+poweroff
